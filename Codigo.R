ce5fef88bdd6e1c26fc9f43bde0de7923e1ed67d
Kelly1202 <70027239+Kelly1202@users.noreply.github.com>
2020-09-17 19:54:46 -0500

    Escribí el encabezado

---
+++ Codigo.R
@@ -0,0 +1,12 @@
+#---
+  #title: '**Sistemas de recomendación como medida para la retención y fidelización de
+ # clientes**'
+#author: "Francisco Pautt, Danisse Carrascal, Kelly Carrascal, Natalia Pérez, Gabriela Muñoz"
+#date: "18/09/2020"
+#output:
+  #pdf_document:
+ # toc: yes
+#word_document:
+ # toc: yes
+#csl: science.csl
+#---
